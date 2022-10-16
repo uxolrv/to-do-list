98e164bea3df0f0b539efd808e12d7764feb1b8e
uxolrv <uxolrv@gmail.com>
2022-10-16 15:18:58 +0900

    :sparkles: feat: 토글 기능, 삭제 기능 구현

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
 import { MdDone, MdDelete } from 'react-icons/md';
+import { useTodoDispatch } from '../TodoContext';
 
 const Remove = styled.div`
     display: flex;
@@ -57,15 +58,22 @@ const Text = styled.div`
 `;
 
 function TodoItem({ id, done, text }) {
+    const dispatch = useTodoDispatch();
+    const onToggle = () => dispatch({ type: 'TOGGLE', id });
+    const onRemove = () => dispatch({ type: 'REMOVE', id });
+
     return (
         <TodoItemBlock>
-            <CheckCircle done={done}>{done && <MdDone/>}</CheckCircle>
+            <CheckCircle done={done} onClick={onToggle}>
+                {done && <MdDone/>}
+            </CheckCircle>
             <Text done={done}>{text}</Text>
-            <Remove>
+            <Remove onClick={onRemove}>
                 <MdDelete />
             </Remove>
         </TodoItemBlock>
     );
 }
 
-export default TodoItem;
+// 다른 항목이 업데이트될 때, 불필요한 리렌더링을 방지 ==> 성능 최적화!
+export default React.memo(TodoItem);
